f321da6e679ff480998f4295f508974acba5320d
zuongminh <actionminh@gmail.com>
2024-04-02 20:58:49 +0700

    second

--- Btec-Website/Program.cs
+++ Btec-Website/Program.cs
@@ -1,7 +1,16 @@
+
+using Btec_Website.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
